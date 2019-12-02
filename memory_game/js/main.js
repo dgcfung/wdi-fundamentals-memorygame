d1f6d0b4926d46587f7c69c44e53a0c343ae908d
B Legit <d.gc.fung@gmail.com>
2019-12-02 18:22:35 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,30 @@
 console.log("Up and Running!");
 
-const cards = ["Queen","Queen","King","King"];
+//const cards = ["Queen","Queen","King","King"];
+
+var cards =[
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: "images/king-of-diamonds.png",
+}
+]
+
 const cardsInPlay = [];//empty array to hold cards in play
 
 function checkForMatch(){
@@ -12,8 +36,11 @@ function checkForMatch(){
 }
 
 function flipCard(cardID){
-console.log("User Flipped " + cards[cardID]);
-cardsInPlay.push(cards[cardID]);
+console.log("User Flipped " + cards[cardID].rank);
+cardsInPlay.push(cards[cardID].rank);
+
+console.log(cards[cardID].cardImage);
+console.log(cards[cardID].suit);
 
 console.log(cardsInPlay);//use to check if card pushed
 console.log(cardsInPlay.length);//use to see cards inside cardsInPlay
